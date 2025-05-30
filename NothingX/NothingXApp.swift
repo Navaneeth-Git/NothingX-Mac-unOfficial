a732c044eb59ff9b9bab83bfd810e5abbaefee01
QuackityDuck <navaneethnandakumar12@gmail.com>
2025-05-31 03:14:54 +0530

    Initial Commit

---
+++ NothingX/NothingXApp.swift
@@ -0,0 +1,17 @@
+//
+//  NothingXApp.swift
+//  NothingX
+//
+//  Created by Navaneeth on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct NothingXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NothingXTests/NothingXTests.swift
@@ -0,0 +1,17 @@
+//
+//  NothingXTests.swift
+//  NothingXTests
+//
+//  Created by Navaneeth on 5/31/25.
+//
+
+import Testing
+@testable import NothingX
+
+struct NothingXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
